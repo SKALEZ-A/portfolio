1639b2b6c96e2db1610c3d2f27f77aedf058c4dd
SKALEZ-A <94375178+SKALEZ-A@users.noreply.github.com>
2023-03-21 22:28:48 +0100

    updated

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
 									className="white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl text-white px-4 py-2  rounded-md ml-8"
 									href="#"
 								>
-									SkalezDgreat
+									{/* SkalezDgreat */}
 								</a>
 							</li>
 							<li>
